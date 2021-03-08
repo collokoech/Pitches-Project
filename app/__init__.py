0165f35bc1932cacd0723e69024e6d492f7afea7
Collo <collokoech4@gmail.com>
2021-03-08 13:03:30 +0300

    added app folder

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '760ebdbdbfdd670f6cf325c6c444e068'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'user.login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 387
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+from app.people.routes import people
+from app.posts.routes import posts
+from app.main.routes import main
+
+
+
+app.register_blueprint(people)
+app.register_blueprint(posts)
+app.register_blueprint(main)
